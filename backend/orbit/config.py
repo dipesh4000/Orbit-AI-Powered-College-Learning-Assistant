b3e866f1aeff5378a75ca68d0da4bf5a1c0d9825
dipesh4000 <dipeshkumar0853822@gmail.com>
2026-09-10 23:55:39 +0530

    fixed the config file to fetch env from project root

--- backend/orbit/config.py
+++ backend/orbit/config.py
@@ -5,7 +5,7 @@ from urllib.parse import urlsplit
 from pydantic import Field, field_validator, model_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-ROOT = Path(__file__).resolve().parents[1]
+ROOT = Path(__file__).resolve().parents[2]
 
 
 class Settings(BaseSettings):
